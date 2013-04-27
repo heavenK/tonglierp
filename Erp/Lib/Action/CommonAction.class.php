ac3c876709b8ed7ced7a1e7d8cfd924b1aa002c8
heavenK <heaven@we54.com>
2013-04-27 09:45:17 +0800

    Signed-off-by: heavenK <heaven@we54.com>

--- Erp/Lib/Action/CommonAction.class.php
+++ Erp/Lib/Action/CommonAction.class.php
@@ -443,7 +443,7 @@ class CommonAction extends Action {
             $id = $_REQUEST [$pk];
             if (isset($id)) {
                 $condition = array($pk => array('in', explode(',', $id)));
-                if (false !== $model->where($condition)->delete()) {
+                if (false !== $model->relation(true)->where($condition)->delete()) {
                     $this->success('删除成功！');
                 } else {
                     $this->error('删除失败！');
