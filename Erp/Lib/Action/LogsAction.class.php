b731beb15f637d8d104ec13e19f45d2c173b8cb2
heavenK <heaven@we54.com>
2013-05-06 13:57:02 +0800

    Signed-off-by: heavenK <heaven@we54.com>

--- Erp/Lib/Action/LogsAction.class.php
+++ Erp/Lib/Action/LogsAction.class.php
@@ -13,7 +13,7 @@ class LogsAction extends CommonAction {
 		
 		$active = 1;
 		$need_role = $this->_post('need_role');
-		
+		dump($_SESSION['roleId']);
 		if($need_role == 7){
 			$project = $Project->where($p_data)->find();
 			if($project['username'] != $_SESSION['loginUserName'] && array_search(9, $_SESSION['roleId']) == FALSE)	$this->error('对不起，您没有这个权限！！');
